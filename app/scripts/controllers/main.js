155a6e2b6d78d241875fe6cdde6f734998bead4b
Seth Bergman <seth.atxwebs@gmail.com>
2016-04-29 12:52:52 -0500

    first

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,28 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name mytodoApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the mytodoApp
+ */
+angular.module('mytodoApp')
+  .controller('MainCtrl', ["$scope", "localStorageService", function ($scope, localStorageService) {
+
+    var todosInStore = localStorageService.get('todos');
+
+    $scope.todos = todosInStore && todosInStore.split('\n') || [];
+
+    $scope.$watch('todos', function () {
+    localStorageService.add('todos', $scope.todos.join('\n'));
+    }, true);
+
+     $scope.addTodo = function () {
+      $scope.todos.push($scope.todo);
+        $scope.todo = '';
+    };
+    $scope.removeTodo = function (index) {
+      $scope.todos.splice(index,1);
+    };
+  }]);
